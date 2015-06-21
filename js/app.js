e5feeac414350a88c6662e75428f90adc94ecca3
zu5462 <zu5462@gmail.com>
2015-06-21 23:17:01 +0800

    a

--- js/app.js
+++ js/app.js
@@ -113,7 +113,7 @@
              $('.portfolio-modal #app').eq(Photonum).text(camera);
              $('.portfolio-modal #style').eq(Photonum).text(style);
              $('.portfolio-modal .tip').eq(Photonum).text(tips);
-             $('.portfolio-modal .photo').eq(Photonum).attr({src:img,height:360px,width:360px});
+             $('.portfolio-modal .photo').eq(Photonum).attr({src:img,height:"360",width:"360"});
              $('.portfolio-item .photo').eq(Photonum).attr("src",img);  
             };
 
