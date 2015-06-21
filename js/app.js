6daeb08006647181864930c7480d8f0dad758c48
zu5462 <zu5462@gmail.com>
2015-06-21 15:58:48 +0800

    aa

--- js/app.js
+++ js/app.js
@@ -113,7 +113,7 @@
              $('.portfolio-modal #app').eq(Photonum).text(camera);
              $('.portfolio-modal #style').eq(Photonum).text(style);
              $('.portfolio-modal .tip').eq(Photonum).text(tips);
-             $('.portfolio-modal .img').eq(Photonum).html('<img src='+img+'class="img-responsive img-centered" alt="">');
-             $('.portfolio-item .img').eq(Photonum).html('<img src='+img+'class="img-responsive img-centered" alt="">');
+             $('.portfolio-modal img').eq(Photonum).html('<img src='+img+'class="img-responsive img-centered" alt="">');
+             $('.portfolio-item img').eq(Photonum).html('<img src='+img+'class="img-responsive img-centered" alt="">');
 
             };
