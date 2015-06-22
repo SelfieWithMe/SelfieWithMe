355d264c30c1eb855ba724096a925856713e2fb0
zu5462 <zu5462@gmail.com>
2015-06-22 22:27:03 +0800

    aa

--- js/key.js
+++ js/key.js
@@ -1,24 +1,3 @@
 //Parse key
 Parse.initialize("3Ct29PJCet8L8kfhLvbLTJ3DLKRC61RAp7ysXFU6", "3yHxjy4EBgJTMWYBuP1T7rG4qOHiiOuAnokZB5jb");
 
-//FB key 
-/*
-window.fbAsyncInit = function() {
- Parse.FacebookUtils.init({
-      appId      : '376624765869068',
-      status     : true,  // check Facebook Login status
-      cookie     : true,  // enable cookies to allow Parse to access the session
-      xfbml      : true, 
-      version    : 'v2.3'
-    });
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-*/
