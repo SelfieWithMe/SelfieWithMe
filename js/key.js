36c3e273835b085dd06427342fd9eb022b39191a
zu5462 <zu5462@gmail.com>
2015-06-22 22:13:29 +0800

    aaa

--- js/key.js
+++ js/key.js
@@ -1,7 +1,8 @@
 //Parse key
 Parse.initialize("3Ct29PJCet8L8kfhLvbLTJ3DLKRC61RAp7ysXFU6", "3yHxjy4EBgJTMWYBuP1T7rG4qOHiiOuAnokZB5jb");
 
-//FB key
+//FB key 
+/*
 window.fbAsyncInit = function() {
  Parse.FacebookUtils.init({
       appId      : '376624765869068',
