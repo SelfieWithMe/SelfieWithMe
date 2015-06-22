e8361828e093480a1be9efa2010d3a0bfd70ed10
zu5462 <zu5462@gmail.com>
2015-06-22 22:56:57 +0800

    dsa

--- js/app.js
+++ js/app.js
@@ -230,7 +230,7 @@
                  $(modalname).find('.photo').attr("src",img);
                  $(itemname).find('a').attr("href","#"+objectId);
                  $(modalname).find('.comment').html('<div class="fb-comments" data-href="http://selfiewithme.github.io/selfiewithme/#'+objectId+'" data-numposts="5"></div>');
-                 $(modalname).find('.likebutton').html('<button type="button" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-star" id="'+objectId+'" aria-hidden="true">'+value+'</span></button>');
+                 $(modalname).find('.likebutton').html('<button type="button" class="btn btn-default btn-lg button'+photonum+'"><span class="glyphicon glyphicon-star" id="'+objectId+'" aria-hidden="true">'+value+'</span></button>');
                  $(itemname).show();
                  $(itemname).find('.photo').attr("src",img);
                  $(itemname).find('.photo').css({'max-height':'360px','max-width':'360px'});
@@ -249,12 +249,13 @@
                };
               setphoto();
             };
-
-          $(document).on('click',location.hash,function(e){
+            
+          for(var i=1;i<7;i++){
+          $(document).on('click','button1',function(e){
             e.preventDefault();
             value(this.id);
             console.log('click'+this.id);
-          });
+          });};
 
             function value(id){
               var Photo = Parse.Object.extend("Photo") ;
