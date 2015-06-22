a10f9047e004788a06ebf8cff27262f7b530f2ac
zu5462 <zu5462@gmail.com>
2015-06-22 20:29:28 +0800

    dsa

--- js/app.js
+++ js/app.js
@@ -232,7 +232,6 @@
                  $(modalname).find('.comment').html('<div class="fb-comments" data-href="http://selfiewithme.github.io/selfiewithme/#'+objectId+'" data-numposts="5"></div>');
                  $(itemname).find('.photo').attr("src",img);
                  $(itemname).find('.photo').css({'max-height':'360px','max-width':'360px'});
-                 console.log(objectId);
                };
 
                function removephoto(){
